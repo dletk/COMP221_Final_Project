aa153ed0738df9e86451278cbfbd9d84794313e1
Duc Le <duc.mtle@gmail.com>
2016-12-08 20:55:43 -0600

    Commit for knapsack_solving and testing

--- src/KnapsackProblem/Knapsack_solving.java
+++ src/KnapsackProblem/Knapsack_solving.java
@@ -1,7 +1,50 @@
 package KnapsackProblem;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * Created by DucLe on 11/28/16.
  */
 public class Knapsack_solving {
+    private HashMap<Integer, ArrayList<Integer>> itemDicts;
+    private int capacity;
+    private int[][] knapsack_table;
+    private int value_max;
+
+    public Knapsack_solving(HashMap<Integer, ArrayList<Integer>> itemDicts, int capacity) {
+        this.itemDicts = itemDicts;
+        this.capacity = capacity;
+        knapsack_table = new int[itemDicts.size() + 1][capacity + 1];
+        value_max = solving();
+    }
+
+    protected int solving() {
+        for (int row = 0; row <= itemDicts.size(); row++) {
+            for (int col = 0; col <= capacity; col++) {
+                if (row == 0 || col == 0) {
+                    knapsack_table[row][col] = 0;
+                } else {
+                    int weightItem = itemDicts.get(row).get(0);
+                    int valueItem = itemDicts.get(row).get(1);
+                    if (col - weightItem >= 0) {
+                        knapsack_table[row][col] =
+                                Integer.max(knapsack_table[row - 1][col],
+                                        valueItem + knapsack_table[row - 1][col - weightItem]);
+                    } else {
+                        knapsack_table[row][col] = knapsack_table[row-1][col];
+                    }
+                }
+            }
+        }
+        return knapsack_table[itemDicts.size()][capacity];
+    }
+
+    public int[][] getKnapsack_table() {
+        return knapsack_table;
+    }
+
+    public int getValue_max() {
+        return value_max;
+    }
 }
---
+++ test/KnapsackProblem/testKnapsack_solving.java
@@ -0,0 +1,27 @@
+package KnapsackProblem;
+
+import org.junit.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by DucLe on 12/8/16.
+ */
+public class testKnapsack_solving {
+    @Test
+    public void testConstructor() {
+        assertNotNull(new Knapsack_solving(new HashMap<Integer, ArrayList<Integer>>(), 10));
+    }
+
+    @Test
+    public void testSolving() {
+        Knapsack_generator problem = new Knapsack_generator(5);
+        Knapsack_solving solver = new Knapsack_solving(problem.getItems_dict(), problem.getCapacity());
+        System.out.println(solver.getValue_max());
+        System.out.println(problem.getCapacity());
+        System.out.print(problem.getItems_dict());
+    }
+}
